c175910395559ce8a8512963ec0728d1d3e108d2
abublebleble <araujo227guimaraes@gmail.com>
2024-11-14 16:34:47 -0300

    coisa

--- resources/views/treinos/create.blade.php
+++ resources/views/treinos/create.blade.php
@@ -2,6 +2,7 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <meta charset="UTF-8">
     <title>Criar Treino</title>
 </head>
@@ -13,7 +14,7 @@
         <label for="nome_treino">Nome do Treino:</label>
         <input type="text" name="nome_treino" required>
 
-        <button type="submit">Criar Treino</button>
+        <button type="submit" >Criar Treino</button>
     </form>
     <a href="{{ route('treinos.index') }}">Voltar</a>
 </body>
